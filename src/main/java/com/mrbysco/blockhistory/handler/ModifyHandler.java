bf129f42c290aa5e93fe86407a80041cba9e3e0e
Mrbysco <Mrbysco@users.noreply.github.com>
2024-07-15 15:55:47 +0200

    Allow null player #9

--- src/main/java/com/mrbysco/blockhistory/handler/ModifyHandler.java
+++ src/main/java/com/mrbysco/blockhistory/handler/ModifyHandler.java
@@ -3,9 +3,11 @@ package com.mrbysco.blockhistory.handler;
 import com.mrbysco.blockhistory.helper.MiscHelper;
 import com.mrbysco.blockhistory.storage.ChangeStorage;
 import com.mrbysco.blockhistory.storage.UserHistoryDatabase;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -32,12 +34,14 @@ public class ModifyHandler {
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public void onBlockBreak(final BlockEvent.BlockToolModificationEvent event) {
 		if (!event.getLevel().isClientSide()) {
-			Entity player = event.getPlayer();
-			final Level level = player.level();
-			if (!MiscHelper.matchesWhitelist(level))
-				return;
+			Player player = event.getPlayer();
+			LevelAccessor level = event.getLevel();
+			if (level instanceof ServerLevel serverLevel) {
+				if (!MiscHelper.matchesWhitelist(serverLevel))
+					return;
+			}
 
-			String name = player.getName().getString();
+			String name = player != null ? player.getName().getString() : "Unknown";
 			if (event.getFinalState() != null) {
 				String extraData = ForgeRegistries.ITEMS.getKey(event.getHeldItemStack().getItem()).toString();
 				ChangeStorage changeData = new ChangeStorage(MiscHelper.getDate(), name, "modify", ForgeRegistries.BLOCKS.getKey(event.getState().getBlock()), extraData);
